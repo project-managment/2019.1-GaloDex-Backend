9fe5f84de3d11d32100cd23c234caaeea971da2b
Pedro "Coin" Duarte <coinbr@gmail.com>
2019-05-16 11:33:58 -0300

    Create test

---
+++ api/status/controllers/Status.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Status.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Status`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve status records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx, next, { populate } = {}) => {
+    if (ctx.query._q) {
+      return strapi.services.status.search(ctx.query);
+    } else {
+      return strapi.services.status.fetchAll(ctx.query, populate);
+    }
+  },
+
+  /**
+   * Retrieve a status record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    return strapi.services.status.fetch(ctx.params);
+  },
+
+  /**
+   * Count status records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx, next, { populate } = {}) => {
+    return strapi.services.status.count(ctx.query, populate);
+  },
+
+  /**
+   * Create a/an status record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.status.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an status record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.status.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an status record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.status.remove(ctx.params);
+  }
+};
